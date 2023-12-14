55a87cc78365d42e7c8d9929384e86f3e8ce2fc4
andrusrem <andrusremets@gmail.com>
2023-12-14 12:14:16 +0200

    Components in TODOList

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,50 @@
 <script setup>
-    import { ref, reactive } from 'vue';
+    import { ref, reactive, computed } from 'vue';
+    import ItemList from './components/ItemList.vue';
     let message = ref('Hello Vuuuuueeeeeee');
     let count = ref(0);
     let newitem = ref('item');
+    let i = 0;
     let items = reactive([
-        'leib',
-        'viin',
-        'piim'
+        {id:i++, name:'kommit', isDone: false},
+        {id:i++, name:'olu', isDone: false},
+        {id:i++, name:'piim', isDone: false},
     ]);
 
     function AddItem()
     {
         if(newitem.value.trim() !== '')
         {
-            items.push(newitem.value);
+            items.push({id:i++ ,name: newitem.value.trim(), isDone:false});
         }
         newitem.value = "";
     }
+    const doneItems = computed(()=>{
+        return items.filter(item => item.isDone);
+    });
+    const notDoneItems = computed(()=>{
+        return items.filter(item => !item.isDone);
+    });
 </script>
 
 <template>
+
     <div class="container">
-        {{ message }} 
-        <button class="button is-primary" @click="count++">Count {{ count }}</button>
-       <!--<input type="text" class="input mt-2" :value="message" @input="message=$event.target.value">--> 
-       <input type="text" v-model="message" class="input mt-2">
-        {{ message.split('').reverse().join('') }}
-        <input type="text" v-model="newitem" class="input mt-2" @keydown.enter="AddItem()">
-        <button class="button is-primary" @click="AddItem()">Add Shit</button>
-        <div class="content">
-            <ul>
-                <li v-for="item in items">
-                    {{ item }}
-                </li>
-            </ul>
+        <div class="field has-addons m-2">
+            <div class="control is-expanded">
+                <input class="input" type="text" v-model="newitem" @keydown.enter="AddItem">
+            </div>
+            <div class="control">
+                <a class="button is-info"  @click="AddItem">
+                    Add item
+                </a>
+            </div>
         </div>
-    </div>
+        <div class="content">
+            <ItemList title="All Items" :items="items"></ItemList>
+            <ItemList title="Done Items" :items="doneItems"></ItemList>
+            <ItemList title="To Do Items" :items="notDoneItems"></ItemList>
+        </div>
+     </div>
     
 </template>
